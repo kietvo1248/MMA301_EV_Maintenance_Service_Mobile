4474b661b83a03ea9ae5c59d6437c482d27c59f4
NguyenNgocMinh-SE182009 <nguyenngocminh17012004@gmail.com>
2025-10-23 14:28:36 +0700

    first commit

---
+++ src/components/ExComponent.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ExComponent() {
+  return (
+    <View>
+      <Text>ExComponent</Text>
+    </View>
+  )
+}
