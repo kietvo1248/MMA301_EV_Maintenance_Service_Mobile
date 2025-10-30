ce7cca5bb5887a3aac1785f75aa5ac3d68956f2c
NguyenNgocMinh-SE182009 <nguyenngocminh17012004@gmail.com>
2025-10-30 14:42:14 +0700

    Create src for staff

---
+++ src/screens/Staff/AppointmentDetailsScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AppointmentDetailsScreen() {
+  return (
+    <View>
+      <Text>AppointmentDetailsScreen</Text>
+    </View>
+  )
+}
